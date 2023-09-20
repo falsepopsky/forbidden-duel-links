825122c2850a5e4f60e286ec747d277abc121ed7
falsepopsky <falsepopsky@protonmail.com>
2023-09-19 21:33:35 -0300

    fix: add Referrer-Policy header

--- next.config.js
+++ next.config.js
@@ -30,6 +30,7 @@ const nextConfig = {
           { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
           { key: 'Cross-Origin-Resource-Policy', value: 'same-origin' },
           { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+          { key: 'Referrer-Policy', value: 'no-referrer' },
         ],
       },
     ];
